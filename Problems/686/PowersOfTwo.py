ee0ae1bc96802945bff2fa8e4ce075fdea3e9d15
Ubuntu <ubuntu@ip-172-31-35-203.us-east-2.compute.internal>
2020-01-23 23:55:03 +0000

    Still slow

--- Problems/686/PowersOfTwo.py
+++ Problems/686/PowersOfTwo.py
@@ -1,30 +1,30 @@
-n = 5134
-s = str(n)
-print(s[0])
-
+from math import log2
 def P(L, n):
     originalN = n
     s = str(L)
-    exponent = -1
-    count = 0
-    last = -1
-    while n > 0:
-        exponent += 1
-        working = str(2 ** exponent)
-        if len(working) >= len(s):
-            if s == working[:len(s)]:
-                percentDone = (originalN - n) / originalN * 100
-                print("%.2f%% done"%percentDone, end='\r')
-                n -= 1
-                print(str(originalN - n) + "th exponent is " + str(exponent)) 
-                if last == -1:
-                    last = exponent
-                else:
-                    # print ("Difference is: " + str(exponent - last))
-                    last = exponent
-    return exponent
+    exponent = 90
+    current = 2 ** 90
+    a = 2 ** 196
+    b = 2 ** 289
+    c = 2 ** 485
+    multiplierList = [a, b, c]
+    while n > 1:
+        for m in multiplierList:
+            candidate = current * m
+            strCandidate = str(candidate)
+            if strCandidate[:3] == "123":
+                current = candidate
+                # print(str(log2(candidate)))
+                break
+        n -= 1
+        if n % 100 == 0:
+            percent = (originalN - n) / originalN * 100
+            print("%.2f%% done."%percent, end = '\r')
 
-print(str(P(123, 10)))
+    return log2(current)
+
+print("Hi")
+print(str(P(123, 678910)))
 
 
 
